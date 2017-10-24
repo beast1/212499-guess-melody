8da77a89865da3e7beae9931dbbf2be384e38578
Egor <beast.loby@gmail.com>
2017-10-24 15:33:03 +0300

    add(utils/getUserScore) метод подсчета очков игрока в конце игры

---
+++ js/utils/getUserScore/getUserScore.js
@@ -0,0 +1,19 @@
+const getUserScore = (answers, REST_ATTEMPTS) => {
+  let USER_POINTS = answers.reduce((previousValue, currentValue) => {
+    let getAnswerPoints = () => {
+      if (currentValue.passed === true) {
+        if (currentValue.time < 30) {
+          return 2;
+        } else {
+          return 1;
+        }
+      } else {
+        return -2;
+      }
+    };
+    return previousValue + getAnswerPoints();
+  }, 0);
+  return USER_POINTS;
+};
+
+export default getUserScore;
---
+++ js/utils/getUserScore/getUserScore.test.js
@@ -0,0 +1,53 @@
+import assert from 'assert';
+import getUserScore from './getUserScore';
+
+const answers1 = [
+  {passed: false, time: 35},
+  {passed: false, time: 45},
+  {passed: false, time: 35},
+  {passed: false, time: 35},
+  {passed: false, time: 45},
+  {passed: false, time: 45},
+  {passed: false, time: 32},
+  {passed: false, time: 35},
+  {passed: false, time: 35},
+  {passed: false, time: 35}
+];
+const answers2 = [
+  {passed: true, time: 25},
+  {passed: true, time: 25},
+  {passed: true, time: 25},
+  {passed: true, time: 25},
+  {passed: true, time: 25},
+  {passed: true, time: 25},
+  {passed: true, time: 12},
+  {passed: true, time: 25},
+  {passed: true, time: 25},
+  {passed: true, time: 25}
+];
+const answers3 = [
+  {passed: true, time: 35},
+  {passed: true, time: 45},
+  {passed: true, time: 35},
+  {passed: true, time: 35},
+  {passed: true, time: 45},
+  {passed: true, time: 45},
+  {passed: true, time: 32},
+  {passed: true, time: 35},
+  {passed: true, time: 35},
+  {passed: false, time: 25}
+];
+
+describe(`getUserScore на выходе отдаёт кол-во набранных очков`, () => {
+  it(`Если игрок ответил неправильно на первые три вопроса, Ф вернет -4 или меньше(т.к. остальные автоматически неправильные)`, () => {
+    assert(getUserScore(answers1, 0) <= -4);
+  });
+
+  it(`Если игрок ответил на все вопросы правильно и потратил на каждый меньше 30 сек, Ф вернет 20`, () => {
+    assert(getUserScore(answers2, 3) === 20);
+  });
+
+  it(`Если игрок ответил на 9 вопросов правильно и потратил больше 30 сек, Ф вернет 7`, () => {
+    assert(getUserScore(answers3, 3) === 7);
+  });
+});
